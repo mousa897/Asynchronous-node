31eb26b8196674f61b630e4ee586606ebfa930bb
Mousa <mousaalihamadna@gmail.com>
2026-02-14 18:01:08 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const superagent = require('superagent');
+
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) return reject('Could not find the file 😭');
+      resolve(data);
+    });
+  });
+};
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject('could not write the file 😭');
+      resolve('file written successfully');
+    });
+  });
+};
+
+readFilePro(`${__dirname}/dog.txt`)
+  .then((data) => {
+    console.log(`Breed: ${data}`);
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then((res) => {
+    console.log(res.body.message);
+    return writeFilePro('dog-img.txt', res.body.message).then(
+      console.log('random image saved'),
+    );
+  })
+  .catch((err) => {
+    console.log(err);
+  });
